c40fed2c1bce33ef22480b88867811af5da3fe3e
Leslie Cruz <l.cruzh22@gmail.com>
2021-07-02 10:09:02 -0400

    yeah

---
+++ sapling/sapling/journalClass.swift
@@ -0,0 +1,14 @@
+//
+//  journalClass.swift
+//  sapling
+//
+//  Created by  Scholar on 7/1/21.
+//
+
+import UIKit
+class JournalEntry {
+    var writingtext = ""
+    var date = ""
+    var mood = ""
+    
+}
